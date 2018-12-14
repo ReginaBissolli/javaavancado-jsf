d168386860f8dac31b6fcdb7bd7067b7d7a8525c
Regina Celia Bissolli <regina.bissolli@allianz.com.br>
2018-12-14 10:01:54 -0200

    Novo projeto jsf

---
+++ Projeto01_jsf/src/br/com/allianz/bean/LivrosBean.java
@@ -0,0 +1,49 @@
+package br.com.allianz.bean;
+
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+import br.com.allianz.dao.LivrosDao;
+import br.com.allianz.models.Livro;
+
+@ManagedBean(name="beanLivro")
+@RequestScoped
+public class LivrosBean {
+	
+	private Livro livro;
+	public LivrosBean() {
+		if(livro == null) {
+			livro = new Livro(); 
+		}
+	}
+		
+	public Livro getLivro() {
+		return livro;
+	}
+
+	public void setLivro(Livro livro) {
+		this.livro = livro;
+	} 
+	
+	//metodo de acao (action) que, qando executado, retorna o redirecionamento conforme o resultado da execução
+	public String incluirLivro() {
+		try {
+			new LivrosDao().incluirLivro(livro); 
+			return "sucesso"; 
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "erro"; 
+		}
+	}
+
+	//propriedade usada para apresentar a lisa dos livros cadastrados 
+	public List<Livro> getListaLivros() throws Exception {
+		return new LivrosDao().listarLivros();
+	}
+	
+	
+	
+	
+}
